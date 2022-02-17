40077755323c60730bd8be2083f48587e409b641
loctet <elvis.konjoh@yahoo.com>
2022-02-17 21:47:35 +0100

    feat(kitty): add immutable

--- contracts/SHIBAKITTYNFT.sol
+++ contracts/SHIBAKITTYNFT.sol
@@ -19,8 +19,28 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@imtbl/imx-contracts/contracts/Mintable.sol";
 
-contract ShibaKittyNFT is ERC721URIStorage, Ownable, Pausable  {
+contract ShibaKittyNFT is ERC721URIStorage, Ownable, IMintable, Pausable {
+  // Events
+    event AssetMinted(address to, uint256 id, bytes blueprint);
+    event UpdatedBaseURI(string URI);
+
+    // Addresses
+    address public imx;
+
+    // String
+    string public baseURI;
+
+    // Mappings
+    mapping(uint256 => string) private _tokenURI;
+    mapping(uint256 => bytes) public blueprints;
+
+    modifier onlyIMX() {
+        require(msg.sender == imx, "ShibaKittyNFT: Function can only be called by IMX");
+        _;
+    }
+
   using SafeMath for uint256;
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
@@ -35,8 +55,10 @@ contract ShibaKittyNFT is ERC721URIStorage, Ownable, Pausable  {
   mapping(address => uint) _addressMinted;
   mapping(address => bool) public whitelist;
 
-  constructor() ERC721("ShibaKitty NFT", "SKITY") { 
-      withdrawTo = msg.sender;
+  constructor(address _imx) ERC721("ShibaKitty NFT", "SKITY") {
+        require(_imx != address(0x0), 'ShibaKittyNFT: Treasury address cannot be the 0x0 address');
+        imx = _imx;
+        withdrawTo = msg.sender;
   }
 
   function withdraw() public onlyOwner {
@@ -85,17 +107,37 @@ contract ShibaKittyNFT is ERC721URIStorage, Ownable, Pausable  {
     return MAX_MINT_PER_ADDR - _addressMinted[addr];
   }
 
-  function mint(uint numberOfTokens) external payable whenNotPaused {
-    require((_addressMinted[msg.sender] + numberOfTokens) <= MAX_MINT_PER_ADDR , "This exceeds the max mint limit");
-    require(_tokenIds.current().add(numberOfTokens) <= MAX_SUPPLY, "Mint would exceed total supply");
-    require(NFT_MINT_UNIT_PRICE.mul(numberOfTokens) <= msg.value, "Ether value sent is not correct for the number to mint");
+  function mint(uint quantity) external payable whenNotPaused {
+    require((_addressMinted[msg.sender] + quantity) <= MAX_MINT_PER_ADDR , "This exceeds the max mint limit");
+    require(_tokenIds.current().add(quantity) <= MAX_SUPPLY, "Mint would exceed total supply");
+    require(NFT_MINT_UNIT_PRICE.mul(quantity) <= msg.value, "Ether value sent is not correct for the number to mint");
 
-    for(uint i=0; i < numberOfTokens; i++) {
+    for(uint i=0; i < quantity; i++) {
       _tokenIds.increment();
       uint256 _idx = _tokenIds.current();
       _mint(msg.sender, _idx);
       _setTokenURI(_idx, string(bytes.concat(bytes(Strings.toString(_idx)), ".json")));
     }
-    _addressMinted[msg.sender] = _addressMinted[msg.sender] + numberOfTokens;
+    _addressMinted[msg.sender] = _addressMinted[msg.sender] + quantity;
+  }
+
+  function mintFor(address user, uint256 quantity, bytes calldata mintingBlob) external override onlyIMX {
+      require(quantity == 1, "ShibaKittyNFT: invalid quantity");
+      // require(NFT_MINT_UNIT_PRICE.mul(quantity) <= msg.value, "Ether value sent is not correct for the number to mint");
+      (uint256 id, bytes memory blueprint) = Minting.split(mintingBlob);
+      _mintFor(user, id);
+      blueprints[id] = blueprint;
+      emit AssetMinted(user, id, blueprint);
+  }
+
+  function _mintFor(address to, uint256 id) internal virtual {
+      require(!_exists(id), "ShibaKittyNFT: Token ID Has Been Used");
+      _mint(to, id);
+  }
+
+
+  function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+      require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+      return super.tokenURI(tokenId);
   }
 }
